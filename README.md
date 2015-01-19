e9bc6ea6a64a6625e60145bf0496212a9c2e98bb
Owain Lewis <owain@owainlewis.com>
2015-01-19 19:57:12 +0000

    minor changes with types

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A micro compiler for the FORTH programming language
 
+I wanted to learn more about FORTH and given how little info there is online
+writing an interpreter seemed like a good way to learn something.
+
 ```ocaml
 # Stack.pop(repl "5 6 + 7 8 + +");;
 - : string = "26"
--- forth.ml
+++ forth.ml
@@ -1,3 +1,26 @@
+(* --------------------------------------------------------------------- *)
+(*                                                                       *)
+(* Forth.ml                                                              *)
+(* A micro compiler for a small subset of the FORTH programming language *)
+(*                                                                       *)
+(* --------------------------------------------------------------------- *)
+
+(* Custom stack instance of OCaml native Stack module
+   to add debugging methods etc *)
+module MStack =
+  struct
+    type 'a mstack = ('a list) ref
+    let empty () = ref []
+    let push stack value : unit =
+      let current_stack = !stack in
+      stack := (value :: current_stack)
+    let pop stack =
+      match (!stack) with
+      | [] -> raise (Failure "Empty stack")
+      | x::xs -> (stack := xs); x
+  end
+
+(* FORTH operators *)
 type token =
     ADD
   | SUB
@@ -15,7 +38,19 @@ let lex_token = function
   | "+" -> ADD
   | "*" -> MULT
   | "/" -> DIV
-  | x   -> ATOM x
+  | x   -> match (try_int x) with
+           | Some(i) -> INT i
+           | None -> ATOM x
+
+and try_int token =
+  try
+    Some(int_of_string token)
+  with _ -> None
+
+and try_float token =
+  try
+    Some(float_of_string token)
+  with _ -> None
 
 (* Lex the input into actionable tokens *)
 let lex input =
@@ -25,21 +60,23 @@ let lex input =
   in let lex_result = aux [] (tokenize input) in
   List.rev lex_result
 
+(* Operations and FORTH built in functions *)
+
 (* Apply the + function to our stack *)
 let add stack =
-  let a = Stack.pop stack
-  and b = Stack.pop stack in
-  let result = (int_of_string a) + (int_of_string b) in
-  Stack.push (string_of_int result) stack
+  let a = MStack.pop stack
+  and b = MStack.pop stack in
+  match (a,b) with
+  | (INT x, INT y) -> MStack.push stack (INT (x + y))
+  | _              -> raise (Failure "Incompatible types for addition operator")
 
 let run stack input =
-  let tokens = tokenize input in
+  let tokens = lex input in
   let _ = List.iter (fun c ->
-              if c = "+" then
-                add(stack)
-              else Stack.push c stack) tokens
+              if c = ADD then add(stack)
+              else MStack.push stack c) tokens
   in stack
 
-let repl input =
-  let stack = Stack.create() in
-  run stack input
+let repl input stack =
+  let result = run stack input in
+  result
