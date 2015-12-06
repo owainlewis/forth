a1c62abaec8b0d11e21cad77420279c96fcb483b
Owain Lewis <owain@owainlewis.com>
2015-12-06 13:20:43 +0000

    updates stack operations

--- forth.ml
+++ forth.ml
@@ -1,4 +1,5 @@
-(* --------------------------------------------------------------------- *)
+(*
+--------------------------------------------------------------------- *)
 (*                                                                       *)
 (* Forth.ml #load "str.cma"                                              *)
 (*                                                                       *)
@@ -6,38 +7,74 @@
 (*                                                                       *)
 (* --------------------------------------------------------------------- *)
 
-open MutableStack
-
-(* Rename to Env *)
-module Assoc = struct  
-    let procs = ref [ ]
-    let insert k v =
-      let existing = !procs in
-        procs := (k, v) :: existing
-
-    let find p = List.assoc p !procs
+(*****************************************************************)
+(* A global environment. This is essentially a dict for words *)
+(*****************************************************************)
+module Env = struct  
+  let env = ref [ ]
+  let insert k v =
+    let existing_env = !env in
+    env := (k, v) :: existing_env				   
+  let find p = List.assoc p !env
 end
 
-(***********************************************)
+(*****************************************************************)
+(* Stack implementation *)
+(*****************************************************************)
+
+exception UnderflowException of string
+			
+module Stack = struct
+  
+  type 'a stack = 'a list ref		     
+  (* Create an empty stack *)			    
+  let empty () = ref []		     
+  (* Peek the top of the stack *)
+  let peek stack = match !stack with
+  | [] -> None
+  | x::_ -> Some(x)		  
+  (* Push an item onto the stack *)		  
+  let push stack value =
+    let new_stack = (value :: !stack) in
+    stack := new_stack	       
+  (* Pop an item off the stack. Unsafe *)	       
+  let pop stack = match (!stack) with
+  | []    -> raise (UnderflowException "Stack underflow")
+  | x::xs -> let _ = stack := xs in x
+  (* Initialize the stack with some values *)					
+  let init_with xs = ref xs		   
+  (* Dump the results of the stack for inspection *)					 
+  let inspect stack = !stack
+end
+
+(*****************************************************************)
 (* STACK OPS *)
-(***********************************************)
+(*****************************************************************)
 
-let init = MutableStack.empty
-
-let swap stack =
-  let a = MutableStack.pop stack
-  and b = MutableStack.pop stack in
-  let _ = List.iter (fun x -> MutableStack.push stack x) [ a; b ] in
+(* Swap the top two elements on the stack *)		 
+let swap (stack : 'a list ref) =
+  let a = Stack.pop stack
+  and b = Stack.pop stack 
+  and f = Stack.push stack in 
+  let _ = List.iter f [ a; b ] in
   !stack
-
 (* Duplicate the next item on the stack *)
 let dup stack =
-  let element = MutableStack.peek stack in
+  let element = Stack.peek stack in
   match element with
-  | Some(e) -> MutableStack.push stack e
+  | Some(e) -> Stack.push stack e
   | None    -> ()
-
-(***********************************************)
+(* Drop an item from the stack *)
+let drop stack =
+  let drop_head = function
+    | [] -> []
+    | x::xs -> xs	      
+  in let updated = List.rev (drop_head (List.rev !stack)) in ref updated
+								 
+let over stack = stack
+let rot stack = stack
+  
+(*****************************************************************)	       
 
 (* FORTH operators *)
 type token =
@@ -110,13 +147,13 @@ let dump stack =
   in print_endline ( "[ " ^ parts ^ " ]" )
 
 let pop2 stack =
-  let a = MutableStack.pop stack
-  and b = MutableStack.pop stack in
+  let a = Stack.pop stack
+  and b = Stack.pop stack in
   (a,b)
 		   
 let apply_prim_op op stack =
   match (pop2 stack) with
-  | (INT x, INT y) -> MutableStack.push stack (INT (op x y))
+  | (INT x, INT y) -> Stack.push stack (INT (op x y))
   | _              -> raise (Failure "Incompatible types for operation")
 
 let add stack  = apply_prim_op (fun x y -> x + y) stack
@@ -128,7 +165,7 @@ let print_forth line = print_endline (line ^ "\nok.")
 
 (* Pop an item off the top of the stack and print it *)
 let dot stack =
-  let element = MutableStack.pop stack in
+  let element = Stack.pop stack in
   print_forth (show_token element)
 
 let run stack program =
@@ -143,15 +180,15 @@ let run stack program =
                 | DUMP  -> dump(stack)
                 | DUP   -> dup(stack)
                 | DOT   -> dot(stack)
-                | _     -> MutableStack.push stack c)
+                | _     -> Stack.push stack c)
       tokens
   in stack
 
 (* RUN A PROCEDURE *)
 let run_proc stack name =
-    let procedure = Assoc.find name in
+    let procedure = Env.find name in
     let _ = run stack procedure in
     stack
 
-let go program = run (MutableStack.empty()) program
+let go program = run (Stack.empty()) program
 
--- stack.ml
+++ stack.ml
@@ -1,41 +1,57 @@
-module type MUTABLE_STACK =
-  sig
-    type 'a mstack
-    val empty : unit -> 'a mstack
-    val push : 'a mstack -> 'a -> unit
-    val pop : 'a mstack -> 'a option
-  end
+(*****************************************************************)
+(* Stack implementation *)
+(*****************************************************************)
 
-module S : MUTABLE_STACK =
-  struct
-    type 'a mstack = ('a list) ref
-    let empty () : 'a mstack = ref []
-    let push (s: 'a mstack) x = s := x :: (!s)
-    let pop s =
-      match (!s) with
-      | [] -> None
-      | x::xs -> let _ = s := xs in Some x
-end
-
-module MutableStack = struct
+exception UnderflowException of string
+			
+module Stack = struct
   
-  type 'a stack = ('a list) ref
-			     
-  let empty () = ref []
-
-  let peek stack =
-    match !stack with | [] -> None | x :: _ -> Some(x)
-
+  type 'a stack = 'a list ref		     
+  (* Create an empty stack *)			    
+  let empty () = ref []		     
+  (* Peek the top of the stack *)
+  let peek stack = match !stack with
+  | [] -> None
+  | x::_ -> Some(x)		  
+  (* Push an item onto the stack *)		  
   let push stack value =
     let new_stack = (value :: !stack) in
-    stack := new_stack
-
-  let pop stack =
-    match (!stack) with
-    | []    -> raise (Failure "Empty stack")
-    | x::xs ->
-       let _ = stack := xs in
-       x
-
+    stack := new_stack	       
+  (* Pop an item off the stack. Unsafe *)	       
+  let pop stack = match (!stack) with
+  | []    -> raise (UnderflowException "Stack underflow")
+  | x::xs -> let _ = stack := xs in x
+  (* Initialize the stack with some values *)					
+  let init_with xs = ref xs		   
+  (* Dump the results of the stack for inspection *)					 
   let inspect stack = !stack
 end
+
+(*****************************************************************)
+(* STACK OPS *)
+(*****************************************************************)
+
+(* Swap the top two elements on the stack *)		 
+let swap (stack : 'a list ref) =
+  let a = Stack.pop stack
+  and b = Stack.pop stack 
+  and f = Stack.push stack in 
+  let _ = List.iter f [ a; b ] in
+  !stack
+(* Duplicate the next item on the stack *)
+let dup stack =
+  let element = Stack.peek stack in
+  match element with
+  | Some(e) -> Stack.push stack e
+  | None    -> ()
+(* Drop an item from the stack *)
+let drop stack =
+  let drop_head = function
+    | [] -> []
+    | x::xs -> xs	      
+  in let updated = List.rev (drop_head (List.rev !stack)) in ref updated
+								 
+let over stack = stack
+let rot stack = stack
+  
+(*****************************************************************)
